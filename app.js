ce77f0a4a871d647582f18bc0a00ec74ca6c3b07
Gabriel Ataide <gabriel.ataide07@gmail.com>
2021-08-09 11:11:48 -0300

    generating colors with chroma.js

---
+++ app.js
@@ -0,0 +1,35 @@
+//global selections and variables
+const colorDivs = document.querySelectorAll('.color');
+const generateBtn = document.querySelector('.generate');
+const sliders = document.querySelectorAll("input[type='range']");
+const currentHexes = document.querySelectorAll('.color h2');
+let initialColors;
+
+//functions
+
+//color generator
+function generateHex() {
+    //  /the code using chroma js
+    const hexColor = chroma.random();
+    return hexColor;
+
+    //  /the code using just js
+    // const letters = '0123456789ABCDEF';
+    // let hash = '#';
+    // for (let i = 0; i < 6; i++) {
+    //     hash += letters[Math.floor(Math.random() * 16)];
+    // }
+    // return hash;
+}
+
+function randomColors() {
+    colorDivs.forEach((div) => {
+        const hexText = div.children[0];
+        const randomColor = generateHex();
+
+        //add the color to the background
+        div.style.backgroundColor = randomColor;
+        hexText.innerText = randomColor;
+    });
+}
+randomColors();
