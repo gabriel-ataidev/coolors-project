c79a90b7becd1856fa92b6e65dbbb8eb1f428e5d
Gabriel Ataide <gabriel.ataide07@gmail.com>
2021-08-11 11:38:08 -0300

    adding the copy function

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const colorDivs = document.querySelectorAll('.color');
 const generateBtn = document.querySelector('.generate');
 const sliders = document.querySelectorAll("input[type='range']");
 const currentHexes = document.querySelectorAll('.color h2');
+const popup = document.querySelector('.copy-container');
 let initialColors;
 
 //event listeners
@@ -13,6 +14,16 @@ colorDivs.forEach((div, index) => {
     div.addEventListener('change', () => {
         updateTextUI(index);
     });
+});
+currentHexes.forEach(hex => {
+    hex.addEventListener('click', () => {
+        copyToClipboard(hex);
+    })
+});
+popup.addEventListener('transitionend', () => {
+    const popupBox = popup.children[0];
+    popup.classList.remove('active');
+    popupBox.classList.remove('active');
 });
 
 //functions
@@ -132,5 +143,17 @@ function resetInputs(){
         }
        
     })
+}
+function copyToClipboard(hex) {
+    const el = document.createElement('textarea');
+    el.value = hex.innerText;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    //pop up animation
+    const popupBox = popup.children[0];
+    popup.classList.add('active');
+    popupBox.classList.add('active');
 }
 randomColors();
