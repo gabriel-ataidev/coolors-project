0cab1c5b24da0a75ac3932834f436ff6d2fd4d85
Gabriel Ataide <gabriel.ataide07@gmail.com>
2021-08-10 23:43:41 -0300

    fixing some things

--- app.js
+++ app.js
@@ -31,9 +31,12 @@ function generateHex() {
     // return hash;
 }
 function randomColors() {
+    initialColors = [];
     colorDivs.forEach((div) => {
         const hexText = div.children[0];
         const randomColor = generateHex();
+        //add it to the array
+        initialColors.push(chroma(randomColor).hex());
 
         //add the color to the background
         div.style.backgroundColor = randomColor;
@@ -83,7 +86,7 @@ function hslControls(e) {
     const brightness = sliders[1];
     const saturation = sliders[2];
 
-    const bgColor = colorDivs[index].querySelector('h2').innerText;
+    const bgColor = initialColors[index];
 
     let color = chroma(bgColor)
     .set('hsl.s', saturation.value)
